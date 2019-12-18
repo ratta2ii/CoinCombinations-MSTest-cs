1b4ff6d217246244de10e0fd671e19ce77d6f691
Misaki Koonce and Trevor Mackin <trevor@>
2019-12-17 16:25:19 -0800

    Add a method to validate the user input.

--- CoinCombinations/Program.cs
+++ CoinCombinations/Program.cs
@@ -8,13 +8,30 @@ namespace CoinCombinations.Models
     {
         static void Main()
         {
-            System.Console.WriteLine("Enter an amount of cents.");
-            int userInput = int.Parse(Console.ReadLine());
+            
+            int userInput = isNumberEntry();
+
+
             Coin newCoin = new Coin(userInput);
             newCoin.CountCoins();
             Console.WriteLine(newCoin.DisplayResults());
 
         }
-    }
+
+        public static int isNumberEntry()
+        {
+            int number = 0;
+            Console.WriteLine("Enter an amount of cents.");
+            bool userInput = Int32.TryParse(Console.ReadLine(), out number);
+
+            if (!userInput || number < 0 || number > 1000000)
+            {
+                Console.WriteLine("Please enter a number between 1 and a million!");
+                number = 0;
+                return isNumberEntry();
+            }
+            return number;
+        }
+    }  
 }
 
